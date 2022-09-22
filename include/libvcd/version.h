9b44b174829860e4425a92eb2897173a6d19898a
Deepin Developer <deepin-dev@deepin.org>
2022-09-22 20:51:44 +0800

    feat: Init commit
    init vcdimager
    
    Log:

---
+++ include/libvcd/version.h
@@ -0,0 +1,11 @@
+/* $Id$ */
+/** \file version.h 
+ *  \brief  A file simply containing the library version number.
+ */
+
+/*! LIBVCD_VERSION can as a string in programs to show what version is used. */
+#define LIBVCD_VERSION "2.0.1"
+
+/*! LIBVCD_VERSION_NUM  can be used for testing in the C preprocessor */
+#define LIBVCD_VERSION_NUM 1
+
